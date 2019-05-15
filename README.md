de7da5e6d1b24edf47e1324a6bdb55e880467985
Jerome Miranda <jerome.m.miranda@gmail.com>
2019-05-15 09:30:48 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Column Email
+# @volenday/column-email
 
+> React Table column for email.
+
+## Install
+
+Using npm:
+
+```sh
+npm install --save column-email
+```
+
+or using yarn:
+
+```sh
+yarn add column-email
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default props => {
+	const {
+		editable = false,
+		format = [],
+		headerStyle = {},
+		id,
+		onChange,
+		onChangeText,
+		style = {},
+		...defaultProps
+	} = props;
+
+	return {
+		...defaultProps,
+		style: { ...style, display: 'flex', alignItems: 'center' },
+		headerStyle: { ...headerStyle, display: 'flex', alignItems: 'center' },
+		Cell: ({ index, original, value }) => {
+			if (editable) {
+				return (
+					<input
+						type="email"
+						class="form-control"
+						value={value}
+						onChange={e => onChangeText(index, id, e.target.value)}
+						onKeyDown={e => {
+							if (e.key === 'Enter') {
+								onChange({ Id: original.Id, [id]: e.target.value });
+							}
+							return;
+						}}
+					/>
+				);
+			} else {
+				return <span>{value}</span>;
+			}
+		},
+		Filter: ({ filter, onChange }) => {
+			return (
+				<input
+					type="email"
+					class="form-control"
+					onChange={e => onChange(e.target.value)}
+					value={filter ? filter.value : ''}
+					placeholder="Search..."
+				/>
+			);
+		}
+	};
+};
